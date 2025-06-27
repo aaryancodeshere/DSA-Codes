320821b602c4cfef0f9f4c4b5f3df134c6c7a90e
Aryan Raj <124273822+aaryancodeshere@users.noreply.github.com>
2025-06-27 23:15:47 +0530

    Time: 0 ms (100%), Space: 23.6 MB (54.22%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -7,7 +7,14 @@ public:
         int n = nums.size();
 
         for(int i=0; i<n; i++){
-            if(count1==0 && nums[i]!=ele2){
+            
+            if(ele1 == nums[i]){
+                count1++;
+            }
+            else if(ele2 == nums[i]){
+                count2++;
+            }
+            else if(count1==0 && nums[i]!=ele2){
                 ele1=nums[i];
                 count1 = 1;
             }
@@ -15,17 +22,12 @@ public:
                 ele2 = nums[i];
                 count2 = 1;
             }
-            else if(ele1 == nums[i]){
-                count1++;
-            }
-            else if(ele2 == nums[i]){
-                count2++;
-            }
             else{
                 count1--;
                 count2--;
             }
         }
+        cout<<count1 << " "<<count2;
         int c1=0,c2=0;
 
         for(int i=0; i<n; i++){
